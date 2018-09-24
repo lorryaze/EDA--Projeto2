be0f29e087df3afccb8772ad53997028f83ab624
Mikhaelle <mikhabueno@yahoo.com>
2018-09-24 17:01:11 -0300

    conjunto treinamento e teste de asfalto

--- Projeto.cpp
+++ Projeto.cpp
@@ -12,7 +12,10 @@ int main (int argc, char *argv[]){
 	int n = 50;
 	int *array;
 	char* sarray = (char *)calloc(n,sizeof(char));
-	char *treina_asfalto[25];
+	char *treina_asfalto[50];
+	char *teste_asfalto[25];
+	char *treina_grama[25];
+	char *teste_grama[25];
 	
 	array = randomizar(n);
 	
@@ -22,24 +25,55 @@ int main (int argc, char *argv[]){
 	}
 	
 
-	for (i =0; i <(n/2); i++){
+	for (i =0; i <n; i++){
 			
 			char numero[] = " ";
 			numero[0] = ((*(array+i))/10) + '0';
 			int num = ((*(array+i))/10)*10;
 			numero[1] = ((*(array+i))-num) + '0';
-			printf("%d\n", *(array+i));
-			char part1[] = "DataSet\\asphalt\\asphalt_";
+			
 			char part2[] = ".txt";
-			char * strcaminho_asfalto = concat(part1, numero, part2); 
-
-			treina_asfalto[i]=strcaminho_asfalto;
-		
+			char part1asfalto[] = "DataSet\\asphalt\\asphalt_";
+			char part1grama[] = "DataSet\\grass\\grass_";
+			char * strcaminho_asfalto = concat(part1asfalto, numero, part2); 
+			if (i <25){
+				treina_asfalto[i]=strcaminho_asfalto;
+			}
+			else{
+				teste_asfalto[i-25]=strcaminho_asfalto;
+			}
 	}
-	
+
+	/*	for (i =0; i <n; i++){
+			
+			char numero[] = " ";
+			numero[0] = ((*(array+i))/10) + '0';
+			int num = ((*(array+i))/10)*10;
+			numero[1] = ((*(array+i))-num) + '0';
+			
+			char part2[] = ".txt";
+			char part1grama[] = "DataSet\\grass\\grass_";
+			char * strcaminho_grama = concat(part1grama, numero, part2);
+			if (i <25){
+				treina_grama[i]=strcaminho_grama;
+			}
+			else{
+				treina_grama[i-25]=strcaminho_grama;
+			}
+			free(strcaminho_grama);
+	}	*/
 	for (i =0; i<25; i++){
 		printf("%s\n", *(treina_asfalto+i));
 	}
+	printf("agr o teste:\n");
+		for (i =0; i<25; i++){
+		printf("%s\n", *(teste_asfalto+i));
+	}
+	//	printf("agr é grama treina:\n");
+	//	for (i =0; i<25; i++){
+	//	printf("%s\n", *(treina_grama+i));
+//	}
+	
 	
 	free(array);
 	free(sarray);
@@ -71,5 +105,6 @@ char* concat( char *s1,  char *s2,  char *s3)
     strcpy(result, s1);
     strcat(result, s2);
     strcat(result, s3);
+    
     return result;
 }
