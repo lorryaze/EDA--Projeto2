613bbd1a7ef00590fe917f04429d91e04174a411
Mikhaelle <mikhabueno@yahoo.com>
2018-09-24 02:44:12 -0300

    add casting int to char

--- teste2.cpp
+++ teste2.cpp
@@ -10,18 +10,18 @@ int main (int argc, char *argv[]){
 	int i = 0;
 	int n = 50;
 	int *array;
+	char* sarray = (char *)calloc(n,sizeof(char));
 	
 	array = randomizar(n);
 	
-	char *array1 = (char*) array;
-	
-	while(array[i] != '\0'){
-		printf("%d \n", array1[i]);
- 		i=i+4;
+	for(i=0; i<50; i++){
+		sarray[i] = *(array+i);
+		printf("%d\n", *(sarray+i));
 	}
 	
+	
 	for(i=0; i<n;i++){
-		printf("%d\n", array[i]);
+		printf("%d\n", *(array+i));
 	}
 	
 	free(array);
@@ -29,13 +29,13 @@ int main (int argc, char *argv[]){
 }
 
 int * randomizar(int n){
-	srand(time(0)); 
+	//srand(time(0)); 
 	int i;
     int * array = (int *) calloc (n, sizeof (int));
-    for (i = 1; i < 51; ++i) {
-        array[i] = i;
+    for (i = 0; i < n; ++i) {
+        array[i] = i+1;
     }
- 	for ( i = 1; i < 51; i++) {    // shuffle array
+ 	for ( i = 0; i < n; i++) {    
     	int temp = array[i];
    		int randomIndex = rand() % n;
   		array[i]= array[randomIndex];
@@ -43,6 +43,7 @@ int * randomizar(int n){
 	}
     return array;
 }
+
 
 
 
